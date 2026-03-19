bf982cbdb71b8ba3602fba9853439027a2fdaaca
Roger Vega <vega.roger@hotmail.com>
2026-03-19 10:19:57 -0500

    Fix: Permite guardar nuevas categorías y no permite duplicados

--- api/components/category/controller.js
+++ api/components/category/controller.js
@@ -22,19 +22,29 @@ module.exports = function(injectedStore) {
     }
 
     async function saveCategory(userId, category){
-        let query = "select c.* from category c, wallet w " +
-            " where c.wallet_id = w.id and w.user_id = ? and c.id = ?";
-        let results = await store.personalizedQuery(query, [userId, category.id]);
-        if(results[0]){
-            return store.update(TABLE, {name: category.name}, {id: category.id});
-        }else{
-            let wallet = await store.query('wallet', {user_id: userId});
-            if(wallet[0]) return store.insert(TABLE, {
-                name: category.name,
-                type: category.type,
-                wallet_id: category.walletId
-            });
+        // Actualizar
+        if(category.id) {
+            let query = "select c.* from category c, wallet w " +
+                " where c.wallet_id = w.id and w.user_id = ? and c.id = ?";
+            let results = await store.personalizedQuery(query, [userId, category.id]);
+            if(results[0]){
+                return store.update(TABLE, {name: category.name}, {id: category.id});
+            }
+            return null;
         }
+        // Nuevo registro - verifica que no haya duplicados
+        let query = "select c.* from category c, wallet w " +
+            " where c.wallet_id = w.id and w.user_id = ? and c.type = ? and c.name = ? and w.id = ?";
+        let results = await store.personalizedQuery(query, [userId, category.type, category.name, category.walletId]);
+
+        if(results[0]) return null;
+
+        let wallet = await store.query('wallet', {user_id: userId});
+        if(wallet[0]) return store.insert(TABLE, {
+            name: category.name,
+            type: category.type,
+            wallet_id: category.walletId
+        });
         return null;
     }
 
