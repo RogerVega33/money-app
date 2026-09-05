f69eb828eb225f07791e1d218bb4fa090b466a47
Roger Vega <vega.roger@hotmail.com>
2026-09-05 14:35:36 -0500

    Improved: Ocultamiento de información sensible en logs

--- network/logger.js
+++ network/logger.js
@@ -27,30 +27,35 @@ function writeLog(log) {
 }
 
 function writeResponse(res){
-    let log = `[${getCurrentTime()}] [response]:${JSON.stringify(res)}`;
-    log = replaceSensitiveInformation(log);
+    let log = `[${getCurrentTime()}] [response]:${serializeForLog(res)}`;
     console.log(log);
     writeLog(log);
 }
 
 function writeRequest(req, res){
     var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || req.ip || null;
-    let log = `[${getCurrentTime()}] [ip]:${ip} [method]:${req.method}:${req.url} [status]:${res.statusCode} [user-agent]:${req.headers['user-agent']} [accept-encoding]:${req.headers['accept-encoding']} [content-type]:${req.headers['content-type']} [request]:${JSON.stringify(req.body)}`;
-    log = replaceSensitiveInformation(log);
+    let log = `[${getCurrentTime()}] [ip]:${ip} [method]:${req.method}:${req.url} [status]:${res.statusCode} [user-agent]:${req.headers['user-agent']} [accept-encoding]:${req.headers['accept-encoding']} [content-type]:${req.headers['content-type']} [request]:${serializeForLog(req.body)}`;
     console.log(log);
     writeLog(log);
 }
 
-function replaceSensitiveInformation(log){
-    let regex = /"password" *: *".*"/;
-    log = log.replace(regex, '"password": "*****"');
-    regex = /"token" *: *".*"/;
-    log = log.replace(regex, '"token": "*****"');
-    regex = /"oldPassword" *: *".*"/;
-    log = log.replace(regex, '"oldPassword": "*****"');
-    regex = /"newPassword" *: *".*"/;
-    log = log.replace(regex, '"newPassword": "*****"');
-    return log;
+// Normalizar los nombres cubre tanto camelCase como snake_case.
+const sensitiveFields = new Set([
+    'password',
+    'oldpassword',
+    'newpassword',
+    'confirmpassword',
+    'token',
+    'accesstoken',
+    'refreshtoken',
+    'recoveryphrase',
+]);
+
+function serializeForLog(value) {
+    return JSON.stringify(value, (key, fieldValue) => {
+        const normalizedKey = key.replace(/_/g, '').toLowerCase();
+        return sensitiveFields.has(normalizedKey) ? '*****' : fieldValue;
+    });
 }
 
 module.exports = {
